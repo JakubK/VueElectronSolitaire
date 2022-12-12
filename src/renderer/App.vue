b9053b088d9fb8661efcc3ce5d3d65f036c5cf45
Jakub Kuniszewski <you@example.com>
2022-12-12 23:25:48 +0100

    Move cell to a separate component

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,26 +1,59 @@
-<script setup lang="ts">
-window.electron.ipcRenderer.send('message', 'Hello from App.vue!');
-</script>
-
 <template>
+  isWon: {{ isWon }}
+  <br/>
+  isLost: {{ isLost }}
+
   <div class="grid">
     <div class="grid__row">
       <div class="grid__corner grid__corner--left"></div>
       <!-- 6 elements -->
-      <div class="grid__element" v-for="index in 6" :key="index">{{ index }}</div>
+      <Cell v-for="index in 3"
+        :key="index"
+        :x="index"
+        :y="1"/>
+      <Cell v-for="index in 3"
+        :key="index"
+        :x="index"
+        :y="2"/>
       <div class="grid__corner grid__corner--right"></div>
     </div>
     <div class="grid__row">
       <!-- 21 elements -->
-      <div class="grid__element" v-for="index in 21" :key="index">{{ index }}</div>
+      <Cell v-for="index in 7"
+        :key="index"
+        :x="index"
+        :y="3"/>
+      <Cell v-for="index in 7"
+        :key="index"
+        :x="index"
+        :y="4"/>
+      <Cell v-for="index in 7"
+        :key="index"
+        :x="index"
+        :y="5"/>
     </div>
     <div class="grid__row">
       <div class="grid__corner grid__corner--left"></div>
       <!-- 6 elements -->
-      <div class="grid__element" v-for="index in 6" :key="index">{{ index }}</div>
+      <Cell v-for="index in 3"
+        :key="index"
+        :x="index"
+        :y="6"/>
+      <Cell v-for="index in 3"
+        :key="index"
+        :x="index"
+        :y="7"/>
       <div class="grid__corner grid__corner--right"></div>
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import Cell from './Cell.vue';
+import { useMap } from './map';
+
+const { isLost, isWon } = useMap();
+
+</script>
 
 <style scoped src="./App.css"/>
---
+++ src/renderer/Cell.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="cell">
+        {{x}}
+        <br/>
+        {{y}}
+    </div>
+</template>
+
+<script lang="ts" setup>
+    defineProps({
+        x: {
+            type: Number,
+            required: true
+        },
+        y: {
+            type: Number,
+            required: true
+        }
+    });
+
+</script>
+
+<style scoped>
+    .cell {
+        border: 1px solid black;
+        aspect-ratio: 1;
+    }
+</style>
