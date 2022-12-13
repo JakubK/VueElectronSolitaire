30b29e80cb5077d4df377d649083e77857f3a648
Jakub Kuniszewski <you@example.com>
2022-12-13 13:55:05 +0100

    Add Cell styling and link click handler from composable

--- src/renderer/Cell.vue
+++ src/renderer/Cell.vue
@@ -1,28 +1,52 @@
 <template>
-    <div class="cell">
-        {{x}}
-        <br/>
-        {{y}}
+    <div 
+        class="cell" 
+        :class="{'cell--taken' : isCellTaken(x,y), 'cell--selected' : isCellSelected(x,y)}" 
+        @click="handleClickCell(x,y)">
+            {{x}}
+            <br/>
+            {{y}}
     </div>
 </template>
 
 <script lang="ts" setup>
-    defineProps({
-        x: {
-            type: Number,
-            required: true
-        },
-        y: {
-            type: Number,
-            required: true
-        }
-    });
+import { useMap } from './map';
+defineProps({
+    x: {
+        type: Number,
+        required: true
+    },
+    y: {
+        type: Number,
+        required: true
+    }
+});
 
+const { isCellSelected, isCellTaken, handleClickCell } = useMap();
 </script>
 
 <style scoped>
     .cell {
         border: 1px solid black;
         aspect-ratio: 1;
+
+        cursor: pointer;
+
+        transition: all 0.5s;
+        transform: scale(1);
     }
+
+    .cell:hover {
+        transform: scale(0.9);
+    }
+
+
+    .cell--taken {
+        background: rgb(78,201,176);
+    }
+
+    .cell--selected {
+        border: 5px solid blue;
+    }
+
 </style>
